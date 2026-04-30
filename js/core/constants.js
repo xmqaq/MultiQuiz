43789e3f58398ab06575b956b768ce4fc9a197b6
xmqaq <2218678594@qq.com>
2026-04-30 15:48:52 +0800

    chore: 清理旧版静态页面代码

--- js/core/constants.js
+++
@@ -1,3 +0,0 @@
-// 存储键与跨模块常量
-const SIDEBAR_COLLAPSE_KEY = 'sidebarCollapsed';
-const BROWSE_ANSWER_MODE_KEY = 'browseAnswerMode';
--- script.js
+++
@@ -1,6 +0,0 @@
-// MultiQuiz 启动入口
-if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', initApp);
-} else {
-    initApp();
-}
